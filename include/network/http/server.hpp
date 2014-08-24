cf298f28cd0c4c94ed42aa2d8d01be05924af9ef
kaoru2010 <k.yanase.2009@gmail.com>
2014-08-24 11:54:20 +0000

    ソケットをlistenする処理とHTTPのリクエストを処理する部分を分離した。

--- include/network/http/server.hpp
+++ include/network/http/server.hpp
@@ -1,17 +1,32 @@
 #pragma once
 
 #include <boost/asio.hpp>
+#include <network/http/context.hpp>
 
 namespace network { namespace http {
 
+template <typename ConnectionContextCreator>
 class server
 {
     boost::asio::io_service& io_service_;
+    boost::asio::ip::tcp::acceptor acceptor_;
+    ConnectionContextCreator& creator_;
 
 public:
-    server(boost::asio::io_service& io_service)
+    server(boost::asio::io_service& io_service, ConnectionContextCreator& creator)
     :   io_service_(io_service)
+    ,   acceptor_(io_service)
+    ,   creator_(creator)
     {}
+
+    void listen() {
+        socket_context<ConnectionContextCreator> context(acceptor_, creator_);
+        context();
+    }
+
+    boost::system::error_code stop(boost::system::error_code &ec) {
+        return acceptor_.close(ec);
+    }
 };
 
 }} // namespace network { namespace http {
--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,64 @@
+#include <boost/asio.hpp>
+#include <boost/asio/coroutine.hpp>
 #include <network/http/server.hpp>
+
+#include <boost/asio/yield.hpp>
+template <typename AsyncReadStream, typename Handler>
+class connection_context : boost::asio::coroutine
+{
+    AsyncReadStream& istream_;
+    Handler handler_;
+    boost::shared_ptr<boost::asio::streambuf> buffer_;
+
+public:
+    connection_context(AsyncReadStream& istream, Handler&& handler)
+    :   istream_(istream)
+    ,   handler_(handler)
+    ,   buffer_()
+    {}
+
+    void operator()(boost::system::error_code ec = {}, std::size_t size = 0) {
+        run(ec, size);
+
+        if (is_complete())
+            handler_();
+    }
+
+private:
+    void run(boost::system::error_code ec, std::size_t size) {
+        reenter(this) {
+            if (ec)
+                return;
+
+            buffer_.reset(new boost::asio::streambuf());
+
+            // Read status line
+            yield boost::asio::async_read_until(istream_, *buffer_, "\r\n", *this);
+
+            // Read header
+            yield boost::asio::async_read_until(istream_, *buffer_, "\r\n\r\n", *this);
+        }
+    }
+};
+#include <boost/asio/unyield.hpp>
+
+class connection_context_creator
+{
+public:
+    template <typename AsyncReadStream, typename Handler>
+    auto create(AsyncReadStream& istream, Handler&& handler) -> connection_context<AsyncReadStream, Handler> {
+        return {istream, handler};
+    }
+};
 
 int main()
 {
+    connection_context_creator creator;
+
     boost::asio::io_service io_service;
-    network::http::server server(io_service);
+    network::http::server<connection_context_creator> server{io_service, creator};
+
+    server.listen();
 
     return 0;
 }
